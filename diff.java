141f7a39b95bb66abf912ff6733b71bbdc6aaef6
Danish Qureshi <cosmicdanish@outlook.com>
2022-10-04 16:18:54 +0530

    new things

--- diff.java
+++ diff.java
@@ -1,7 +1,9 @@
 public class diff {
     public static void main(String[] args) {
         String s = "danish is amazing";
+        System.out.println("danish");
         System.out.println(s);
+        
     }
     
 }
